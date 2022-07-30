d8ac7a22147ff32767e20e0d9ad7ace540909eaa
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-07-30 16:50:37 +0100

    Correction

--- src/components/Errors/NotConnected.jsx
+++ src/components/Errors/NotConnected.jsx
@@ -21,7 +21,7 @@ const NotConnected = () => {
             
         </div>
 
-        <p className={classes.note}><b>Note: </b>Hupay Wallet is not yet available for Mobile users</p>
+        <p className={classes.note}><b>Note: </b>Hupay Wallet is not yet available for Mobile users, but you can <span>Receive HUC on mobile by adding the Toke to your metamask App</span></p>
     </div>
   )
 }
